12835def445aa92cd9f16cc3e93362ad7daca132
Calvet Yann <71167181+CalvetYann@users.noreply.github.com>
2022-01-13 11:32:47 +0100

    Potis avancements

--- src/app/brands-list/brand-new/brand-new.page.ts
+++ src/app/brands-list/brand-new/brand-new.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
+import { BrandsService } from 'src/app/brands.service';
+import { Brands } from 'src/app/models/brands.model';
 
 @Component({
   selector: 'app-brand-new',
@@ -6,10 +10,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./brand-new.page.scss'],
 })
 export class BrandNewPage implements OnInit {
+  public brand!: Brands;
 
-  constructor() { }
+  constructor(
+    private Brand: BrandsService,
+    private toastCtrl: ToastController,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.brand = new Brands();
+  }
+
+  async presentToast() {
+    const toast = this.toastCtrl.create({
+      message: 'Brand successfully added!',
+      duration: 2000
+    });
+    (await toast).present().then(() => {
+      setTimeout(() => {
+        this.router.navigate(['tabs/brands']);
+      }, 2000);
+    });
+  }
+
+  add() {
+    this.Brand.addBrand(this.brand).subscribe(() => {
+      this.brand = new Brands();
+      this.presentToast();
+    });
   }
 
 }
--- src/app/ships-list/ship-new/ship-new.page.ts
+++ src/app/ships-list/ship-new/ship-new.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
+import { Ships } from 'src/app/models/ships.model';
+import { ShipsService } from 'src/app/ships.service';
 
 @Component({
   selector: 'app-ship-new',
@@ -6,10 +10,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ship-new.page.scss'],
 })
 export class ShipNewPage implements OnInit {
+  public ship!: Ships;
 
-  constructor() { }
+  constructor(
+    private Ship: ShipsService,
+    private toastCtrl: ToastController,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.ship = new Ships();
+  }
+
+  async presentToast() {
+    const toast = this.toastCtrl.create({
+      message: 'Ship successfully added!',
+      duration: 2000
+    });
+    (await toast).present().then(() => {
+      setTimeout(() => {
+        this.router.navigate(['tabs/ships']);
+      }, 2000);
+    });
+  }
+
+  add() {
+    this.Ship.addShip(this.ship).subscribe(() => {
+      this.ship = new Ships();
+      this.presentToast();
+    });
   }
 
 }
